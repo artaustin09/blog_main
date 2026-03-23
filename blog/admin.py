4d3d21bae07ed34eede1aac021e1f9361435dde4
artaustin09 <a.r.t.austin09@gmail.com>
2026-03-23 16:03:32 -0700

    Actualizacion del commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
-from  .models import Post, category, comment
-# Register yours models here.
+from .models import Post, category, comment
 
 
-admin.site.register(Post)
-admin.site.register(category)
-admin.site.register(comment)
+class CommentItemInline(admin.TabularInline):
+    model = comment
+    raw_id_fields = ['post']
 
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ['category', 'created_at', 'status']
+    inlines = [CommentItemInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ('title',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(category, CategoryAdmin)
+admin.site.register(comment, CommentAdmin)
 
